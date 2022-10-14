ed67f909c2276b770e478c0022477ffa24510cbd
Szasz Tamas-Csaba <szaszdex@gmail.com>
2022-10-14 15:43:23 +0300

    Moving the immersion game to engine version 0.4

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import './style.css'
-import { quickStart, Vector3, Engine } from 'surreal-engine';
+import { quickStart, Vector3, Engine, DirectionalLight, GlobalLight, Box, offsetCameraMod, keyboardMotionMod, MotionPlugin } from 'surreal-engine';
+import { EngineOpts } from 'core/engine';
 
-const opts = {
+const opts: EngineOpts = {
   showFps: true,
 };
 
 const setupCameraAndLighting = (engine: Engine) => {
-  engine.creator.directionalLight({
+  engine.new(DirectionalLight, {
     color: '#ffffff',
     intensity: 1,
     pos: new Vector3(0, 30, 25),
@@ -14,79 +15,59 @@ const setupCameraAndLighting = (engine: Engine) => {
     castShadow: true,
     shadowAreaHeight: 50,
     shadowAreaWidth: 40,
-  })
-  engine.creator.ambientLight({ color: '#ffffff', intensity: 0.5 });
+  });
+  engine.new(GlobalLight, { color: '#ffffff', intensity: 0.5 });
   engine.setOrthographicCamera({
     distance: 12,
   });
 }
 
 quickStart("#app", opts,
-  assets => {
-    assets.setBasePath('/assets/');
-    assets.addTexture('asphalt', 'asphalt/ASPHALT_001_COLOR.jpg');
-    assets.addTexture('asphalt@normal', 'asphalt/ASPHALT_001_NRM.jpg');
-    assets.addTexture('asphalt@bump', 'asphalt/ASPHALT_001_DISP.png');
-    assets.addTexture('asphalt@ao', 'asphalt/ASPHALT_001_OCC.jpg');
+  (assets) => {
+    assets.setBasePath("/assets/");
+    assets.addTexture("grass", "grass/Stylized_Grass_001_basecolor.jpg");
+    assets.addTexture("grass@normal", "grass/Stylized_Grass_001_normal.jpg");
+    assets.addTexture("grass@roughness", "grass/Stylized_Grass_001_roughness.jpg");
+    assets.addTexture("grass@ao", "grass/Stylized_Grass_001_ambientOcclusion.jpg");
+    assets.addTexture("grass@height", "grass/Stylized_Grass_001_height.png");
   },
   engine => {
+    engine.registerPlugin(new MotionPlugin());
+    engine.materials.addTexturedMaterial("grass", {
+      texture: {
+        map: "grass",
+        bumpMap: "grass@height",
+        normalMap: "grass@normal",
+        aoMap: "grass@ao",
+        // roughnessMap: "grass@roughness", TODO: This is not implemented in the engine yet
+      },
+      repeat: {
+        x: 8,
+        y: 8,
+      }
+    });
     setupCameraAndLighting(engine);
 
-    engine.materials.addTexturedMaterial('asphalt', {
-      texture: {
-        map: 'asphalt',
-        normalMap: 'asphalt@normal',
-        bumpMap: 'asphalt@bump',
-        aoMap: 'asphalt@ao',
-      },
-      repeat: { x: 10, y: 1 },
-    });
-
-    engine.creator.box({
-      pos: new Vector3(0, -1, 0),
-      size: new Vector3(80, 1, 8),
-      mass: 0,
-      rigid: true,
-      receiveShadow: true,
-      material: 'asphalt',
-    });
-
-    // Path
-    engine.creator.box({
-      pos: new Vector3(0, -1, 12),
-      size: new Vector3(6, 1, 16),
-      mass: 0,
-      rigid: true,
-      receiveShadow: true,
-      material: 'yellow',
-    });
-
     // Grass
-    engine.creator.box({
-      pos: new Vector3(15, -1, 12),
-      size: new Vector3(24, 1, 16),
+    engine.new(Box, {
+      size: new Vector3(100, 1, 100),
       mass: 0,
       rigid: true,
       receiveShadow: true,
-      material: 'green',
-    });
-    engine.creator.box({
-      pos: new Vector3(-15, -1, 12),
-      size: new Vector3(24, 1, 16),
-      mass: 0,
-      rigid: true,
-      receiveShadow: true,
-      material: 'green',
+      material: 'grass',
     });
 
-    engine.creator.box({
-      pos: new Vector3(-20, 0, 0),
+    engine.new(Box, {
+      pos: new Vector3(0, 5, 0),
       size: new Vector3(1, 1, 1),
       mass: 1,
       rigid: true,
       castShadow: true,
       material: 'red',
-    }).withOffsetCamera(new Vector3(-10, 10, -10))
-    .withKeyboardMotion();
+      mods: [
+        offsetCameraMod(new Vector3(-10, 10, -10)),
+        keyboardMotionMod(1, 1, 1),
+      ],
+    });
   }
 );
